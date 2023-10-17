4e30419c2af06738b656a3c82bfccb8b3d54e85f
huzzainSaheed <agidingbadis@gmail.com>
2023-10-17 03:34:58 -0700

    new update

--- print_functions.c
+++ print_functions.c
@@ -144,7 +144,7 @@ int print_int(va_list types, char buffer[],
 /************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
- * @types: Lista of arguments
+ * @types: List of arguments in the function
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
